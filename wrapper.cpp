35bf366d8a5b6ad344d4b89d4d6a1525108182b9
Jason McGough <j.mcgough227@gmail.com>
2014-12-04 19:25:01 -0500

    set up wrapper menu.

--- wrapper.cpp
+++ wrapper.cpp
@@ -9,20 +9,19 @@
 
 using namespace std;
 
-//"Build a tree|Do something|Load a tree"
 
-int menu_select(string title, string selections)
+int menu_select(string menu_title, string selections)
 {
-  int count =1;
-  int selection;
+  int count = 1;
+  int select;
   
-  cout << "=*=*=*=*=*=*=" << title << "=*=*=*=*=*=*=" << endl << endl;
+  cout << "=*=*=*=*=*=*=" << menu_title << "=*=*=*=*=*=*=" << endl << endl;
   cout << "Please make a selection..." << endl;
   cout << "[1] ";
 
-  for(char c : items)
+  for(char c : selections)
   {
-    if(c == '|') cout << "\n[" << count << "] ";
+    if(c == '|') cout << "\n[" << ++count << "] ";
     else cout << c;
   }
 
@@ -30,15 +29,62 @@ int menu_select(string title, string selections)
 
   do
   {
-    cin >> selection;
-  } while(selection < 1 || selection > count);
+    cin >> select;
+  } while(select < 1 || select > count);
 
-  return selection;
+  return select;
 }
 
-int main(int argc, int* argv[])
+int main(int argc, char* argv[])
 {
+  string menu_title = "AI Project 2: Decision Trees";
+  string selections = "Learn/Load a decision tree|"
+                      "Test Decision tree accuracy|"
+		      "Apply decision tree to new cases|"
+		      "Quit";
+
+  string submenu_title = "Apply decision tree to new cases";
+  string submenu_selections = "Enter a new case interactively|"
+                              "Quit";
   
-  
+  bool program_loop = true;
+  bool submenu_loop;
+  int select;
+
+  while(program_loop)
+  {
+    select = menu_select(menu_title, selections);
+
+    switch(select)
+    {
+      case 1:
+      {
+        // Learn/load tree
+        break;
+      }
+
+      case 2:
+      {
+        // Test tree accuracy
+        break;
+      }
+
+      case 3:
+      {
+        submenu_loop = true;
+
+        while(submenu_loop)
+	{
+          select =  menu_select(submenu_title, submenu_selections);
+            // Enter cases interactively
+            // Quit
+          break;
+	}
+      }
+
+      case 4: program_loop = false;
+    }
+  }
+
   return 0;
 }
